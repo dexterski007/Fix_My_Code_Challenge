f9087617b5ebacc52e45c53df101d77e8e7558e3
Dexterski007 <mbouj007@gmail.com>
2024-05-10 14:10:05 -0700

    waliy

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
 """
+
 
 class User():
     """ Documentation """
@@ -22,7 +23,7 @@ class User():
             raise TypeError("email must be a string")
         self.__email = value
 
-    
+
 if __name__ == "__main__":
 
     u = User()
