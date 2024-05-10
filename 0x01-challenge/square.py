0741452960f088bf81785f195372d451ea8f3fc7
Dexterski007 <mbouj007@gmail.com>
2024-05-10 14:08:10 -0700

    hachaha

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,13 +3,12 @@
 
 
 class square():
-""" square class """    
+    """ square class """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
-	""" init function """
+        """ init function """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -18,15 +17,15 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-	""" perimeter """
+        """ perimeter """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-	""" str function """
+        """ str function """
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
-""" main function """
 
     s = square(width=12, height=9)
     print(s)
